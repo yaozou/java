d319c83ffc6e841dad8d18952896d71a5b217f47
yaozou <771538058@qq.com>
2018-04-17 23:43:36 +0800

    学习底层代码

---
+++ src/main/java/com/yaozou/web/RestTestController.java
@@ -0,0 +1,9 @@
+package com.yaozou.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "")
+public class RestTestController {
+}
---
+++ src/main/java/com/yaozou/web/WebTestController.java
@@ -0,0 +1,9 @@
+package com.yaozou.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class WebTestController {
+}
