8ddc8edbdb3cafc6fbd65d5bbe603cb109012700
yaozou <771538058@qq.com>
2018-04-18 22:52:14 +0800

    设计模式

---
+++ src/main/java/com/yaozou/model/creation/FactoryMethod.java
@@ -0,0 +1,69 @@
+package com.yaozou.model.creation;
+
+/**
+ * 工厂方法模式
+ */
+public class FactoryMethod {
+    public static void main(String[] args) {
+        /**
+         * 普通工厂模式
+         * 建立一个工厂类，对实现了同一接口的一些类进行实例的创建
+         * 如：发送信息，可以短信/邮件/qq/微信等
+         */
+        SenderFactory factory = new SenderFactory();
+        factory.produce("sms").send();
+        /**
+         * 多工厂模式
+         * 对普通工厂方法模式的改进，在普通工厂方法模式中，
+         * 如果传递的字符串出错，则不能正确创建对象，而多个工厂方法模式是提供多个工厂方法，
+         * 分别创建对象
+         */
+        new MultiSenderFactory().produceQQ().send();
+
+        /**
+         * 静态工厂方法模式
+         * 多个工厂方法模式里的方法置为静态的，不需要创建实例，直接调用
+         */
+        StaticSenderFactory.produceQQ().send();
+    }
+}
+
+interface Sender{
+    void send();
+}
+class SmsSender implements Sender{
+    public void send() {
+        System.out.println("this is sms sender");
+    }
+}
+class QQSender implements Sender{
+    public void send() {
+        System.out.println("this is QQ sender");
+    }
+}
+class SenderFactory{
+    public Sender produce(String type){
+        if ("qq".equals(type)) return new QQSender();
+        else if ("sms".equals(type)) return new SmsSender();
+
+        return null;
+    }
+}
+
+class MultiSenderFactory{
+    public Sender produceQQ(){
+        return new QQSender();
+    }
+    public Sender produceSms(){
+        return new SmsSender();
+    }
+}
+
+class StaticSenderFactory{
+    public static Sender produceQQ(){
+        return new QQSender();
+    }
+    public static Sender produceSms(){
+        return new SmsSender();
+    }
+}
---
+++ src/main/java/com/yaozou/model/creation/SingletonModel.java
@@ -0,0 +1,31 @@
+package com.yaozou.model.creation;
+
+/**
+ * 单例模式
+ * 单例对象（Singleton）是一种常用的设计模式。在Java应用中，单例对象能保证在一个JVM中，
+ * 该对象只有一个实例存在。这样的模式有几个好处：
+ 1、某些类创建比较频繁，对于一些大型的对象，这是一笔很大的系统开销。
+ 2、省去了new操作符，降低了系统内存的使用频率，减轻GC压力。
+ 3、有些类如交易所的核心交易引擎，控制着交易流程，如果该类可以创建多个的话，系统完全乱了。
+ （比如一个军队出现了多个司令员同时指挥，肯定会乱成一团），所以只有使用单例模式，才能保证核心交易服务器独立控制整个流程。
+ */
+public class SingletonModel {
+    /* 持有私有静态实例，防止被引用，此处赋值为null，目的是实现延迟加载 */
+    private static SingletonModel model = null;
+    /* 私有构造方法，防止被实例化 */
+    private SingletonModel(){}
+
+    public static SingletonModel getInstance(){
+        if (model == null) syncInit();
+        return model;
+    }
+
+    /* 如果该对象被用于序列化，可以保证对象在序列化前后保持一致 */
+    public Object readResolve() {
+        return model;
+    }
+
+    private synchronized static void syncInit(){
+        if (model == null) model = new SingletonModel();
+    }
+}
