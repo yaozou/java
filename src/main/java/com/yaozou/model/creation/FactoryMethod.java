f8c66a54c8aca53316037c03a3b5fe16c9b96ffd
yao.zou <yao.zou@yanwei365.com>
2018-04-19 20:33:58 +0800

    解sid

--- src/main/java/com/yaozou/model/creation/FactoryMethod.java
+++ src/main/java/com/yaozou/model/creation/FactoryMethod.java
@@ -25,6 +25,16 @@ public class FactoryMethod {
          * 多个工厂方法模式里的方法置为静态的，不需要创建实例，直接调用
          */
         StaticSenderFactory.produceQQ().send();
+
+        /**
+         * 抽象工厂模式
+         * 工厂方法模式有一个问题就是，类的创建依赖工厂类，也就是说，如果想要拓展程序，必须对工厂类进行修改，这违背了闭包原则。
+         * 所以，从设计角度考虑，有一定的问题，如何解决？
+         * 就用到抽象工厂模式，创建多个工厂类，这样一旦需要增加新的功能，直接增加新的工厂类就可以了，
+         * 不需要修改之前的代码。
+         */
+        Sender sender = new QQProduce().produce();
+        sender.send();
     }
 }
 
@@ -64,6 +74,20 @@ class StaticSenderFactory{
         return new QQSender();
     }
     public static Sender produceSms(){
+        return new SmsSender();
+    }
+}
+
+interface Provider{
+    Sender produce();
+}
+class QQProduce implements Provider{
+    public Sender produce() {
+        return new QQSender();
+    }
+}
+class SmsProduce implements Provider{
+    public Sender produce() {
         return new SmsSender();
     }
 }
