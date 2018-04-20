4e9c3c9fe98e210110c4a14f7be2df746f9b6156
yao.zou <yao.zou@yanwei365.com>
2018-04-20 18:04:39 +0800

    map

---
+++ src/main/java/com/yaozou/jdk/TestCollection.java
@@ -0,0 +1,20 @@
+package com.yaozou.jdk;/**
+ * created by yaozou on 2018/4/20
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 集合类
+ * @author yaozou
+ * @create 2018-04-20 17:47
+ **/
+public class TestCollection {
+
+    public void testMap(){
+       Map map = new HashMap();
+       map.put("aa","aaa");
+       map.put("bb","bbb");
+    }
+}
