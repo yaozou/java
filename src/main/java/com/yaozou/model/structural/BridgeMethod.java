6070b4db6390c58c50dd9f4b840f159e9410fe9c
yaozou <771538058@qq.com>
2018-04-22 21:41:21 +0800

    桥接模式

---
+++ src/main/java/com/yaozou/model/structural/BridgeMethod.java
@@ -0,0 +1,63 @@
+package com.yaozou.model.structural;
+
+import lombok.Data;
+
+/**
+ * 桥接模式
+ */
+public class BridgeMethod {
+    public static void main(String[] args) {
+        // 基本思路：桥接模式就是把事物和其具体实现分开，使他们可以各自独立的变化。
+        // 桥接的用意是：将抽象化与实现化解耦，使得二者可以独立变化，
+        /**
+         *  例如：常用的JDBC桥DriverManager一样，JDBC进行连接数据库的时候，在各个数据库之间进行切换，基本不需要动太多的代码，甚至丝毫不用动，
+         *       原因就是JDBC提供统一接口，每个数据库提供各自的实现，用一个叫做数据库驱动的程序来桥接就行了
+         */
+        BridgeMethod method = new BridgeMethod();
+        method.run();
+    }
+
+    interface Sourceable{
+        void method();
+    }
+
+    class SourceSubFirst implements Sourceable{
+        public void method() {
+            System.out.println("This is the first sub");
+        }
+    }
+
+    class SourceSubSecond implements Sourceable{
+        public void method() {
+            System.out.println("This is the second sub");
+        }
+    }
+
+    @Data
+    abstract class Bridge{
+        private Sourceable source;
+        public void method(){
+            source.method();
+        }
+    }
+
+    class MyBridge extends Bridge{
+        public void method(){
+            getSource().method();
+        }
+    }
+
+    public void run(){
+        Bridge bridge = new MyBridge();
+
+        /*调用第一个对象*/
+        Sourceable source1 = new SourceSubFirst();
+        bridge.setSource(source1);
+        bridge.method();
+
+        /*调用第二个对象*/
+        Sourceable source2 = new SourceSubSecond();
+        bridge.setSource(source2);
+        bridge.method();
+    }
+}
