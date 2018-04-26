2504b9f7cfee71d61adbc83c9de0e98657079299
yao.zou <yao.zou@yanwei365.com>
2018-04-26 21:06:37 +0800

    解sid

--- src/main/java/com/yaozou/jdk/TestCollection.java
+++ src/main/java/com/yaozou/jdk/TestCollection.java
@@ -2,6 +2,8 @@ package com.yaozou.jdk;/**
  * created by yaozou on 2018/4/20
  */
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -48,5 +50,22 @@ public class TestCollection {
         List<Integer> linkedList = new LinkedList<Integer>();
         linkedList.add(11);
 
+    }
+
+    public static void main(String[] args){
+        DecimalFormat decimalFormat = new DecimalFormat("###################.##");
+        BigDecimal p2 = new BigDecimal(2.0D);
+        BigDecimal p3 = new BigDecimal(2.00D);
+        BigDecimal p4 = new BigDecimal(2.50D);
+        BigDecimal p5 = new BigDecimal(2.585444D);
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+        System.out.println(p5.toString());
+
+        System.out.println(decimalFormat.format(p2));
+        System.out.println(decimalFormat.format(p3));
+        System.out.println(decimalFormat.format(p4));
+        System.out.println(decimalFormat.format(p5));
     }
 }
