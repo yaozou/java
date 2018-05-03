142cba8d89fb66334c58b92cd1612055b76f62ef
yao.zou <yao.zou@yanwei365.com>
2018-05-03 22:01:25 +0800

    jvm

---
+++ src/main/java/com/yaozou/jdk/TestJvm.java
@@ -0,0 +1,14 @@
+package com.yaozou.jdk;/**
+ * created by yaozou on 2018/5/3
+ */
+
+/**
+ *
+ * @author yaozou
+ * @create 2018-05-03 21:41
+ **/
+public class TestJvm {
+    final int i = 0;
+    static int j =0;
+
+}
