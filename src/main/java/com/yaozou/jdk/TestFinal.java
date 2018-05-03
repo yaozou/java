a067f79f39f56394afdb4c95cc5d9845d21716b7
yao.zou <yao.zou@yanwei365.com>
2018-05-03 21:21:04 +0800

    final

---
+++ src/main/java/com/yaozou/jdk/TestFinal.java
@@ -0,0 +1,18 @@
+package com.yaozou.jdk;/**
+ * created by yaozou on 2018/5/3
+ */
+
+/**
+ *
+ * @author yaozou
+ * @create 2018-05-03 21:04
+ **/
+public class TestFinal {
+    public static void main(String[] args){
+        String a = new String();
+        StringBuffer sb1 = new StringBuffer();
+        sb1.toString();
+        StringBuilder sb2 = new StringBuilder();
+
+    }
+}
