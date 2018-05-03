092f125e598a3e93d83865b998029e98a2047f4f
yao.zou <yao.zou@yanwei365.com>
2018-05-03 21:28:21 +0800

    final

--- src/main/java/com/yaozou/jdk/TestFinal.java
+++ src/main/java/com/yaozou/jdk/TestFinal.java
@@ -9,10 +9,17 @@ package com.yaozou.jdk;/**
  **/
 public class TestFinal {
     public static void main(String[] args){
-        String a = new String();
         StringBuffer sb1 = new StringBuffer();
         sb1.toString();
         StringBuilder sb2 = new StringBuilder();
 
+        String a = "hello2";
+        final String b = "hello";
+        String d = "hello";
+        String c = b + 2;
+        String e = d + 2;
+        System.out.println((a == c));
+        System.out.println((a == e));
     }
+
 }
