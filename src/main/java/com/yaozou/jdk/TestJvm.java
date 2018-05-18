721527fddb18b94e9954c9b75d3eff30bec817d1
yaozou <771538058@qq.com>
2018-05-18 12:20:42 +0800

    redis

--- src/main/java/com/yaozou/jdk/TestJvm.java
+++ src/main/java/com/yaozou/jdk/TestJvm.java
@@ -2,6 +2,7 @@ package com.yaozou.jdk;/**
  * created by yaozou on 2018/5/3
  */
 
+import java.io.File;
 import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
@@ -33,4 +34,50 @@ public class TestJvm {
         PhantomReference<String> pr = new PhantomReference<String>(new String("hello"), queue);
         System.out.println(pr.get());
     }
+
+    Object obj = new Object();
+    public void method1(int i){
+        int j = 0;
+        int sum = i+j;
+        Object abc = obj;
+        long start = System.currentTimeMillis();
+
+        return;// 异常 正常
+    }
+    public void method2(){
+        File file = new File("");
+    }
+    public void method3(){
+        method3();
+    }
 }
+class TestGC{
+    public static TestGC testGC = null;
+    public static void main(String[] args) throws InterruptedException {
+        testGC = new TestGC();
+        testGC = null;
+        System.gc();
+        Thread.sleep(500);
+        if (null != testGC) { //此时对象应该处于(reachable, finalized)状态
+            System.out.println("Yes , I am still alive");
+        } else {
+            System.out.println("No , I am dead");
+        }
+        testGC = null;
+        System.gc();
+        Thread.sleep(500);
+        if (null != testGC) {
+            System.out.println("Yes , I am still alive");
+        } else {
+            System.out.println("No , I am dead");
+        }
+    }
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("execute method finalize()");
+        testGC = this;
+    }
+}
+
+
