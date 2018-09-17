2d73f2e9210f331994f0ff48433419ae780c02f2
yaozou <yaozou@pand-auto.com>
2018-09-17 14:53:32 +0800

    jvm优化计算并发数

---
+++ src/main/java/com/yaozou/thread/ThreadTest.java
@@ -0,0 +1,29 @@
+package com.yaozou.thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Description:
+ * @Auther: yaozou
+ * @Date: 2018/9/17 10:16
+ */
+public class ThreadTest extends Thread {
+    private static final AtomicInteger count = new AtomicInteger();
+
+    public static void main(String[] args) {
+        while (true)
+            (new ThreadTest()).start();
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println(count.incrementAndGet());
+        while (true)
+            try {
+                Thread.sleep(Integer.MAX_VALUE);
+            } catch (InterruptedException e) {
+                break;
+            }
+    }
+}
