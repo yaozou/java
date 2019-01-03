1157fdd5408d916c0998385d95e578177fb2e7bd
yaozou <yaozou@pand-auto.com>
2019-01-03 15:26:41 +0800

    switch示例

---
+++ src/main/java/com/yaozou/jdk/TestSwitch.java
@@ -0,0 +1,20 @@
+package com.yaozou.jdk;
+
+/**
+ * @Description:
+ * @author: yaozou
+ * @Date: 2018/12/20 14:24
+ */
+public class TestSwitch {
+    public static void main(String[] args){
+        int n = 10;
+        switch (n){
+            default:
+                System.out.println("default");
+                break;
+            case 1:
+                System.out.println("1");
+                break;
+        }
+    }
+}
