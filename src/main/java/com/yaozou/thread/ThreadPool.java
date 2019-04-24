386c08a9644a273e0575900b38e81b9aafac8fd4
yaozou <yaozou@pand-auto.com>
2019-04-24 16:10:56 +0800

    线程池

--- src/main/java/com/yaozou/thread/ThreadPool.java
+++ src/main/java/com/yaozou/thread/ThreadPool.java
@@ -19,10 +19,10 @@ public class ThreadPool {
      * @param corePoolSize  核心池大小 默认情况下，在创建好线程池之后，线程池中的线程数为0，当有任务来之后，就会创建一个线程去执行任务，
      *                      当线程池中的线程数量达到corePoolSize后，就会把这些任务放到缓存队列中
      * @param maximumPoolSize 线程池最大线程数量，表示在线程池中最多能创建线程的数量；在corePoolSize和maximumPoolSize的线程数会被自动释放，而小于corePoolSize的则不会。
-     * @param keepAliveTime 表示线程没有执行任务时最多保持多久时间会终止。
+     * @param keepAliveTime 表示线程在没有执行任务时最多保持多久时间会终止。
      *                      默认情况下，只有当线程池中的线程数大于corePoolSize时，keepAliveTime才会生效,直到线程池数量不大于corePoolSize，
      *                      即只有当线程池数量大于corePoolSize数量，超出这个数量的线程一旦到达keepAliveTime就会终止。
-     *                      但是如果调用了allowCoreThreadTimeout(boolean)方法，即使线程池的线程数量不大于corePoolSize，线程也会在keepAliveTime之后就终止，知道线程池的数量为0为止。
+     *                      但是如果调用了allowCoreThreadTimeout(boolean)方法，即使线程池的线程数量不大于corePoolSize，线程也会在keepAliveTime之后就终止，直到线程池的数量为0为止。
      * @param unit 参数keepAliveTime的时间单位，一个时间单位枚举类。 Nanos/Micros/Millis/Seconds/Minutes/Hours/Days
      * @param workQueue 一个阻塞队列，用来存储等待执行任务的队列，这个参数选择也很重要，会对线程池的运行过程产生重大影响。
      *                  一般来说，这里的阻塞队列就是（ArrayBlockingQueue、LinkedBlockingQueue、SynchronousQueue）。
