72d0110cf646809809ddb9547d1cda9134498c0b
yaozou <771539058@qq.com>
2019-09-18 18:10:36 +0800

    同步计数实例

--- src/main/java/com/yaozou/jdk/concurrent/TestSynchronized.java
+++ src/main/java/com/yaozou/jdk/concurrent/TestSynchronized.java
@@ -1,21 +1,107 @@
 package com.yaozou.jdk.concurrent;
 
 class TestSynchronized{
-    public static void main(String[] args){
-        synchronized (TestSynchronized.class){
+    private static int count = 0;
 
-        }
-        System.out.println("over");
+    public static void main(String[] args) throws Exception{
+        System.out.println("-----------一个对象多个线程------------");
+        // 一个对象多个线程
+        oneObjectMultipleThreads();
+
+        System.out.println("-----------多个对象多个线程------------");
+        // 多个对象多个线程
+        multipleObjectsMultipleThreads();
     }
+
+
+    private static void oneObjectMultipleThreads() throws Exception{
+        TestSynchronized testSynchronized = new TestSynchronized();
+
+        for (int n = 1;n<=5;n++){
+            for (int i=0;i<100;i++){
+                final int methodNum = n;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            Thread.sleep(10);
+                        }catch (Exception e){}
+                        switch (methodNum){
+                            case 1:
+                                testSynchronized.method1();
+                                break;
+                            case 2:
+                                testSynchronized.method2();
+                                break;
+                            case 3:
+                                testSynchronized.method3();
+                                break;
+                            case 4:
+                                testSynchronized.method4();
+                                break;
+                            case 5:
+                                testSynchronized.method5();
+                                break;
+                            default:testSynchronized.method1();
+                        }
+                    }
+                }).start();
+            }
+
+            Thread.sleep(1200);
+            System.out.println("method"+n+" count:"+count);
+            count = 0;
+        }
+    }
+
+    private static void multipleObjectsMultipleThreads() throws Exception{
+        for (int n = 1;n<=5;n++){
+            for (int i=0;i<100;i++){
+                final int methodNum = n;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try{
+                            Thread.sleep(10);
+                        }catch (Exception e){}
+                        TestSynchronized testSynchronized = new TestSynchronized();
+                        switch (methodNum){
+                            case 1:
+                                testSynchronized.method1();
+                                break;
+                            case 2:
+                                testSynchronized.method2();
+                                break;
+                            case 3:
+                                testSynchronized.method3();
+                                break;
+                            case 4:
+                                testSynchronized.method4();
+                                break;
+                            case 5:
+                                testSynchronized.method5();
+                                break;
+                            default:testSynchronized.method1();
+                        }
+                    }
+                }).start();
+            }
+
+            Thread.sleep(1200);
+            System.out.println("method"+n+" count:"+count);
+            count = 0;
+        }
+    }
+
     /**
      * 锁实例object对象
      */
+    Object  lock = new Object();
     public void method1(){
-        Object  lock = new Object();
         // 同步代码块，锁住的是配置的实例对象
         // Object作为锁
         synchronized (lock){
-            System.out.println("----method1----");
+            TestSynchronized.count++;
         }
     }
 
@@ -25,7 +111,7 @@ class TestSynchronized{
     public void method2(){
         // 同步代码块，锁住的是该类的实例对象
         synchronized (this){
-            System.out.println("----method2----");
+            TestSynchronized.count++;
         }
     }
 
@@ -35,7 +121,7 @@ class TestSynchronized{
      */
     public void method3(){
         synchronized(TestSynchronized.class){
-            System.out.println("----method3----");
+            TestSynchronized.count++;
         }
     }
 
@@ -45,7 +131,7 @@ class TestSynchronized{
      * 实例方法，锁住的是该类的实例对象
      */
     public synchronized void method4(){
-        System.out.println("----method4----");
+        TestSynchronized.count++;
     }
 
     /**
@@ -53,7 +139,7 @@ class TestSynchronized{
      * 静态方法，锁住的是类对象
      */
     public static synchronized void method5(){
-        System.out.println("----method5----");
+        TestSynchronized.count++;
     }
 }
 
