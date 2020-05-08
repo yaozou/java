d53343c25cba8856569dd54209014e0c9eff5ae3
yaozou <cbyz.0820>
2020-05-08 11:25:20 +0800

    ThreadPoolExecutor

--- src/main/java/com/yaozou/jdk/concurrent/ThreadPool.java
+++ src/main/java/com/yaozou/jdk/concurrent/ThreadPool.java
@@ -1,5 +1,6 @@
 package com.yaozou.jdk.concurrent;
 
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -19,10 +20,8 @@ public class ThreadPool {
      * @param corePoolSize  核心池大小 默认情况下，在创建好线程池之后，线程池中的线程数为0，当有任务来之后，就会创建一个线程去执行任务，
      *                      当线程池中的线程数量达到corePoolSize后，就会把这些任务放到缓存队列中
      * @param maximumPoolSize 线程池最大线程数量，表示在线程池中最多能创建线程的数量；在corePoolSize和maximumPoolSize的线程数会被自动释放，而小于corePoolSize的则不会。
-     * @param keepAliveTime 表示线程在没有执行任务时最多保持多久时间会终止。
-     *                      默认情况下，只有当线程池中的线程数大于corePoolSize时，keepAliveTime才会生效,直到线程池数量不大于corePoolSize，
-     *                      即只有当线程池数量大于corePoolSize数量，超出这个数量的线程一旦到达keepAliveTime就会终止。
-     *                      但是如果调用了allowCoreThreadTimeout(boolean)方法，即使线程池的线程数量不大于corePoolSize，线程也会在keepAliveTime之后就终止，直到线程池的数量为0为止。
+     * @param keepAliveTime 等待工作的空闲线程的超时时间(纳秒)。
+     *                      当存在超过corePoolSize或allowCoreThreadTimeOut(为true)时，线程将使用此超时。否则，他们将永远等待新的工作。
      * @param unit 参数keepAliveTime的时间单位，一个时间单位枚举类。 Nanos/Micros/Millis/Seconds/Minutes/Hours/Days
      * @param workQueue 一个阻塞队列，用来存储等待执行任务的队列，这个参数选择也很重要，会对线程池的运行过程产生重大影响。
      *                  一般来说，这里的阻塞队列就是（ArrayBlockingQueue、LinkedBlockingQueue、SynchronousQueue）。
@@ -64,7 +63,7 @@ class JdkThreadPool{
 
         ThreadPoolExecutor fixedThreadPool = new ThreadPoolExecutor(1, 3,
                 1L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>());
+                 new ArrayBlockingQueue<>(2));
         /*fixedThreadPool.allowCoreThreadTimeOut(true);*/
         int n = 0;
         int maxSize = 5;
@@ -72,7 +71,7 @@ class JdkThreadPool{
             fixedThreadPool.submit(() -> {
                 try {
                     System.out.println(Thread.currentThread().getName());
-                    Thread.sleep(5000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                     Thread.currentThread().interrupt();
@@ -80,11 +79,33 @@ class JdkThreadPool{
             });
         }
 
-        fixedThreadPool.shutdown();
+        long start = System.currentTimeMillis();
+        while (true){
+            if (System.currentTimeMillis() - start > 10000){
+                break;
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+            System.out.println(fixedThreadPool.toString());
+        }
+
+        // 仅执行之前提交的任务，已经执行了shutdown之后不再接受新提交的任务。
+        // 不保证已经提交的任务执行成功
+        /*fixedThreadPool.shutdown();*/
+        // 试图停止所有正在执行的任务
+        // 清空任务队列
+        // 返回所有等待执行的任务
+        /*List<Runnable> list = fixedThreadPool.shutdownNow();*/
+        // 等待所有任务执行成功
         /*try {
-            fixedThreadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+            boolean flag = fixedThreadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
         } catch (InterruptedException e) {
             e.printStackTrace();
+            Thread.currentThread().interrupt();
         }*/
     }
 }
