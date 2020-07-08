c59755e01e11b369f243da6c74dedba90d52fd5b
yaozou <cbyz.0820>
2020-07-08 15:58:02 +0800

    kafka test

---
+++ src/main/java/com/yaozou/mq/kafka/KafkaConsumerTest.java
@@ -0,0 +1,73 @@
+package com.yaozou.mq.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * created on 2020/7/8 14:03
+ *
+ * @author yaozou
+ * @since v1.0.0
+ */
+public class KafkaConsumerTest {
+
+    public static void main(String[] args) throws UnknownHostException {
+        Properties config = new Properties();
+        config.put("client.id", InetAddress.getLocalHost().getHostName());
+        config.put("group.id", "foo");
+        config.put("bootstrap.servers", "192.168.98.90:9092");
+        config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        config.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        List<String> topics = new ArrayList<>();
+        topics.add("monitor");
+
+        KafkaConsumerTest KafkaConsumerTest = new KafkaConsumerTest(config,topics);
+
+        KafkaConsumerTest.run();
+    }
+    private final KafkaConsumer<String, String> consumer;
+    private final List<String> topics;
+    private final AtomicBoolean shutdown;
+    private final CountDownLatch shutdownLatch;
+
+    public KafkaConsumerTest(Properties config, List<String> topics) {
+        this.consumer = new KafkaConsumer<>(config);
+        this.topics = topics;
+        this.shutdown = new AtomicBoolean(false);
+        this.shutdownLatch = new CountDownLatch(1);
+    }
+
+    public void process(ConsumerRecord<String, String> record){
+        System.out.println(record.value());
+    }
+
+    public void run() {
+        try {
+            consumer.subscribe(topics);
+
+            while (!shutdown.get()) {
+                ConsumerRecords<String, String> records = consumer.poll(500);
+                records.forEach(record -> process(record));
+            }
+        } finally {
+            consumer.close();
+            shutdownLatch.countDown();
+        }
+    }
+
+    public void shutdown() throws InterruptedException {
+        shutdown.set(true);
+        shutdownLatch.await();
+    }
+
+}
---
+++ src/main/java/com/yaozou/mq/kafka/KafkaProducerTest.java
@@ -0,0 +1,50 @@
+package com.yaozou.mq.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
+/**
+ * @author yao.zou
+ * @version V1.0
+ * @description:
+ * @date 15:19
+ */
+public class KafkaProducerTest {
+    public static void main(String[] args) {
+        KafkaProducerTest producerTest = new KafkaProducerTest();
+        producerTest.send();
+    }
+    private KafkaProducer<String, String> producer;
+    public KafkaProducerTest(){
+        this.producer = kafkaProducer();
+    }
+
+    public void send(){
+        final ProducerRecord<String, String> record = new ProducerRecord<>("monitor", "test 111111111111111");
+        producer.send(record, (metadata, e) -> {
+            if (e != null) {
+                e.printStackTrace();
+                System.out.println("Kafka Send failed for record "+record);
+            }
+        });
+    }
+
+    private KafkaProducer<String, String> kafkaProducer() {
+        Properties config = new Properties();
+        try {
+            config.put("client.id", InetAddress.getLocalHost().getHostName());
+        } catch (UnknownHostException ignored) {
+        }
+        config.put("bootstrap.servers", "192.168.98.90:9092");
+        config.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        config.put("acks", "all");
+
+        return new KafkaProducer<>(config);
+    }
+}
