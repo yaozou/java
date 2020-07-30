9216543d3871316ce8d041d4a609ab04fa0f51d2
yaozou <cbyz.0820>
2020-07-30 09:54:39 +0800

    queue

---
+++ src/main/java/com/yaozou/jdk/base/TestQueue.java
@@ -0,0 +1,30 @@
+package com.yaozou.jdk.base;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * created on 2020/7/30 9:50
+ *
+ * @author yaozou
+ * @since v1.0.0
+ */
+public class TestQueue {
+    public static void main(String[] args) {
+        // 1. Initialize a queue.
+        Queue<Integer> q = new LinkedList();
+        // 2. Get the first element - return null if queue is empty.
+        System.out.println("The first element is: " + q.peek());
+        // 3. Push new element.
+        q.offer(5);
+        q.offer(13);
+        q.offer(8);
+        q.offer(6);
+        // 4. Pop an element.
+        q.poll();
+        // 5. Get the first element.
+        System.out.println("The first element is: " + q.peek());
+        // 7. Get the size of the queue.
+        System.out.println("The size is: " + q.size());
+    }
+}
