7c609ae9295bcbf62a22fc30d44caf099165ae63
yaozou <cbyz.0820>
2020-07-30 10:44:49 +0800

    queue

---
+++ src/main/java/com/yaozou/algorithm/leetcode/queue/Solution.java
@@ -0,0 +1,25 @@
+package com.yaozou.algorithm.leetcode.queue;
+
+/**
+ * created on 2020/7/30 10:21
+ *
+ * @author yaozou
+ * @since v1.0.0
+ */
+public class Solution {
+
+    public int numIslands(char[][] grid) {
+        int num = 0;
+
+        for (int i = 0;i<grid.length;i++){
+            // loop horizontally
+
+            // loop vertically
+
+            // loop symmetrically
+
+        }
+
+        return num;
+    }
+}
