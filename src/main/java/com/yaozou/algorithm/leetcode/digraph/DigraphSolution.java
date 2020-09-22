09516507ac792fc8a0bc3a98298c3c30584e9470
yaozou <cbyz.0820>
2020-09-22 18:29:26 +0800

    leetcode 有序图

--- src/main/java/com/yaozou/algorithm/leetcode/digraph/DigraphSolution.java
+++ src/main/java/com/yaozou/algorithm/leetcode/digraph/DigraphSolution.java
@@ -76,6 +76,23 @@ public class DigraphSolution {
         System.out.println(array+"  "+str+"  "+reStr+"->"+reStr.equals(str));
     }
 
+    /**
+     * 冗余连接 II
+     * 在本问题中，有根树指满足以下条件的有向图。该树只有一个根节点，所有其他节点都是该根节点的后继。每一个节点只有一个父节点，除了根节点没有父节点。
+     *
+     * 输入一个有向图，该图由一个有着N个节点 (节点值不重复1, 2, ..., N) 的树及一条附加的边构成。附加的边的两个顶点包含在1到N中间，这条附加的边不属于树中已存在的边。
+     *
+     * 结果图是一个以边组成的二维数组。 每一个边 的元素是一对 [u, v]，用以表示有向图中连接顶点 u 和顶点 v 的边，其中 u 是 v 的一个父节点。
+     *
+     * 返回一条能删除的边，使得剩下的图是有N个节点的有根树。若有多个答案，返回最后出现在给定二维数组的答案。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/redundant-connection-ii
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param edges
+     * @return
+     */
+
     public int[] findRedundantDirectedConnection(int[][] edges) {
        // 仅存在一个子节点 父结点，子节点
        Map<Integer,List<Integer>> m1 = new HashMap<>(edges.length);
---
+++ src/main/java/com/yaozou/algorithm/leetcode/tree/MinCameraCover.java
@@ -0,0 +1,35 @@
+package com.yaozou.algorithm.leetcode.tree;
+
+/**
+ * created on 2020/9/22 18:27
+ *
+ * @author yaozou
+ * @since v1.0.0
+ */
+public class MinCameraCover {
+
+    /**
+     * 给定一个二叉树，我们在树的节点上安装摄像头。
+     *
+     * 节点上的每个摄影头都可以监视其父对象、自身及其直接子对象。
+     *
+     * 计算监控树的所有节点所需的最小摄像头数量。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/binary-tree-cameras
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param root
+     * @return
+     */
+    public int minCameraCover(TreeNode root) {
+
+        return 0;
+    }
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
