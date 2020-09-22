73ff5f7f544ea7864feb1561f01b0da0dc74a257
yaozou <cbyz.0820>
2020-09-22 11:37:00 +0800

    leetcode 有序图

--- src/main/java/com/yaozou/algorithm/leetcode/digraph/DigraphSolution.java
+++ src/main/java/com/yaozou/algorithm/leetcode/digraph/DigraphSolution.java
@@ -1,6 +1,8 @@
 package com.yaozou.algorithm.leetcode.digraph;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -35,220 +37,181 @@ public class DigraphSolution {
 
         // [[4,1],[1,5],[4,2],[5,1],[4,3]]   {{4,1},{1,5},{4,2},{5,1},{4,3}}       5->1     (5)
 
+        // [[9,3],[4,9],[3,1],[10,2],[5,4],[6,8],[6,5],[10,6],[2,3],[3,7]]   {{9,3},{4,9},{3,1},{10,2},{5,4},{6,8},{6,5},{10,6},{2,3},{3,7}}       2->3
+
         // [[1,2], [2,3], [3,1]]            {{1,2}, {2,3}, {3,1}} 3->1
 
 
-        System.out.println("-----------------1----------------");
-        execute(new int[][]{{1,2}, {2,3},{3,4},{4,1},{1,5}},"4->1");
+       /* System.out.println("-----------------(1)即是子节点也是两个节点的父结点----------------");
+        execute(new int[][]{{1,2}, {2,3},{3,4},{4,1},{1,5}},"4->1");*/
 
-        System.out.println("-----------------2----------------");
-        execute(new int[][]{{2,1},{3,1},{4,2},{1,4}},"2->1");
-        execute(new int[][]{{1,2}, {1,3}, {2,3}},"2->3");
+        System.out.println("-----------------（2） 即是父结点也是两个节点的子节点----------------");
+        execute(new int[][]{{2,1},{3,1},{4,2},{1,4}},"2->1","{{2,1},{3,1},{4,2},{1,4}}");
+        /*execute(new int[][]{{1,2}, {1,3}, {2,3}},"2->3");
         execute(new int[][]{{4,2},{1,5},{5,2},{5,3},{1,4}},"5->2");
-        execute(new int[][]{{1,5},{3,2},{2,4},{4,5},{5,3}},"4->5");
+        execute(new int[][]{{1,5},{3,2},{2,4},{4,5},{5,3}},"4->5");*/
 
-        System.out.println("-----------------3----------------");
+       /* System.out.println("-----------------（3）有两个节点异常，既有(1)也有(2)，并且（1）情况的节点是三个节点的父结点----------------");
         execute(new int[][]{{1,4},{5,2},{1,3},{4,5},{1,5}},"1->5");
         execute(new int[][]{{4,2},{1,5},{5,2},{4,3},{4,1}},"5->2");
         execute(new int[][]{{5,2},{1,5},{4,2},{4,3},{4,1}},"4->2");
 
-        System.out.println("-----------------4----------------");
+        System.out.println("-----------------（4）有两个节点异常，既有(1)也有(2), 为（2）情况的节点它的一个子节点也是它的父结点----------------");
         execute(new int[][]{{4,2},{1,5},{5,2},{5,3},{2,4}},"4->2");
         execute(new int[][]{{3,5},{1,3},{2,1},{5,4},{2,3}},"2->3");
 
-        System.out.println("-----------------5----------------");
+        System.out.println("-----------------(5) (4)和(3)----------------");
         execute(new int[][]{{4,1},{1,5},{4,2},{5,1},{4,3}},"5->1");
 
         System.out.println("-----------------other----------------");
-       execute(new int[][]{{1,2}, {2,3}, {3,1}},"3->1");
+       execute(new int[][]{{1,2}, {2,3}, {3,1}},"3->1");*/
     }
 
-    private static void execute(int[][] edges,String str){
+    private static void execute(int[][] edges,String str,String array){
         DigraphSolution solution = new DigraphSolution();
         int[] result = solution.findRedundantDirectedConnection(edges);
         String reStr = +result[0]+"->"+result[1];
-        System.out.println(str+"  "+reStr+"->"+reStr.equals(str));
+        System.out.println(array+"  "+str+"  "+reStr+"->"+reStr.equals(str));
     }
 
     public int[] findRedundantDirectedConnection(int[][] edges) {
-       int[] result = new int[2];
-
        // 仅存在一个子节点 父结点，子节点
        Map<Integer,Integer> m1 = new HashMap<>(edges.length);
        // 两个子节点       父结点，子节点
-       Map<Integer,Integer> m2 = new HashMap<>(16);
+       Map<Integer,List<Integer>> m2 = new HashMap<>(16);
 
        // 为一个父结点，一个子结点 子结点，父结点
         Map<Integer,Integer> m3 = new HashMap<>(edges.length);
        // 为一个父结点，两个子结点  子结点，父结点
-        Map<Integer,Integer> m4 = new HashMap<>(16);
+        Map<Integer,List<Integer>> m4 = new HashMap<>(16);
 
-        boolean flag = false;
        for (int i=0;i<edges.length;i++){
            // key1->key2
            int key1 = edges[i][0];
            int key2 = edges[i][1];
 
-           if (m1.containsKey(key1) && m2.containsKey(key1)){
-               flag = true;
-           }else if (!m1.containsKey(key1)){
+           if (!m1.containsKey(key1)){
                 m1.put(key1,key2);
             }else{
-                m2.put(key1,key2);
+               List<Integer> list = m2.getOrDefault(key1,new ArrayList<>());
+               list.add(key2);
+                m2.put(key1,list);
             }
 
             if (!m3.containsKey(key2)){
                 m3.put(key2,key1);
             }else{
-                m4.put(key2,key1);
+                List<Integer> list = m4.getOrDefault(key1,new ArrayList<>());
+                list.add(key1);
+                m4.put(key2,list);
             }
        }
 
-       if (!m2.isEmpty() && !m4.isEmpty()){
-           // (7) 有三个节点异常，既两个有(1)也一个有(2),并且（1）情况的一个节点也是一个子节点
-           if (m2.size() > 1){
+        if (!m4.isEmpty()){
+            // 两个节点的子节点
+            return result1(edges,m1,m2,m3,m4);
+        }
 
-           }
-
-           if (m4.size() > 1){
-
-           }
-
-           // 为两个子节点的父结点       父结点，子节点
-           // 父结点，子节点
-           int[] dif1 = new int[2];
-           for (Map.Entry<Integer, Integer> entry :m2.entrySet()) {
-               dif1[0] = entry.getKey();
-               dif1[1] = entry.getValue();
-               break;
-           }
-
-           // 为一个父结点，两个子结点  子结点，父结点
-           // 父结点，子节点
-           int[] dif2 = new int[2];
-           for (Map.Entry<Integer, Integer> entry :m4.entrySet()) {
-               dif2[0] = entry.getValue();
-               dif2[1] = entry.getKey();
-               break;
-           }
-
-           //（3）有两个节点异常，既有(1)也有(2)，并且（1）情况的节点是三个节点的父结点
-           if (flag){
-               // 双向
-               Integer p1 = m1.get(dif2[1]);
-               if (p1 != null && p1.intValue() == dif2[0]){
-                   result[0] = dif2[0];
-                   result[1] = dif2[1];
-                   return result;
-               }
-               // 闭环
-               if (dif1[0] == dif2[0]){
-                   result[0] = dif1[0];
-                   result[1] = dif2[1];
-               }else {
-                   int g = dif2[1];
-                   for (int i=0;i<edges.length;i++) {
-                       // key1->key2
-                       if (g == edges[i][1]){
-                           result[0] = edges[i][0];
-                           result[1] = g;
-                       }
-                   }
-                   return result;
-               }
-               return result;
-           }
-
-           //（4）有两个节点异常，既有(1)也有(2), 为（2）情况的节点它的一个子节点也是它的父结点
-           if (m1.containsKey(dif2[1])){
-               int val = m1.get(dif2[1]);
-               Integer val2 = m3.get(dif2[1]);
-               if (val2!=null && val2.intValue() == val){
-                   result[0] = val;
-                   result[1] = dif2[1];
-                   return result;
-               }
-           }
-
-           // (6) 有两个节点异常，既有(1)也有(2),并且（1）情况的节点也是一个子节点
-           if (m3.containsKey(dif1[0])){
-               result[0] = m3.get(dif1[0]);
-               result[1] = dif1[0];
-               return result;
-           }
-
-
-           // (5) (4)和(3)
-           int end = dif2[1];
-           // 跟key相关的，取最后一次出现的
-           for (int i=0;i<edges.length;i++) {
-               // key1->key2
-               if (end == edges[i][1]){
-                   result[0] = edges[i][0];
-                   result[1] = end;
-               }
-           }
-           return result;
-       }else if (!m2.isEmpty()){
-           // 为两个子节点的父结点       父结点，子节点
-           // 父结点，子节点
-           int[] dif = new int[2];
-           for (Map.Entry<Integer, Integer> entry :m2.entrySet()) {
-               dif[0] = entry.getKey();
-               dif[1] = entry.getValue();
-               break;
-           }
-           Integer n1 = m1.get(dif[0]);
-           if (n1 != null && m1.get(n1).intValue() == dif[1]){
-              result[0] = n1;
-              result[1] = dif[1];
-               return result;
-           }
-
-
-           if (cricle1(dif,dif[0],m1)){
-              int val =  m1.get(dif[0]);
-              dif[1]  = val;
-           }
-
-           // 跟key相关的，取最后一次出现的
-           for (int i=0;i<edges.length;i++) {
-               // key1->key2
-               int key1 = edges[i][0];
-               int key2 = edges[i][1];
-               if ((key1 == dif[0] || key2 == dif[0]) && (key2 != dif[1])){
-                   result[0] = key1;
-                   result[1] = key2;
-               }
-           }
-       }else if (!m4.isEmpty()){
-           // 为一个父结点，两个子结点  子结点，父结点
-           // 父结点，子节点
-           int[] dif = new int[2];
-           for (Map.Entry<Integer, Integer> entry :m4.entrySet()) {
-               dif[0] = entry.getValue();
-               dif[1] = entry.getKey();
-               break;
-           }
-
-           result[0] = m3.get(dif[1]);
-           // TODO 父结点 取未形成闭环的结点
-           /**
-            *    1 ->5
-            *  / ^
-            * v   \
-            * 2-->3
-            */
-           if (cricle1(dif,dif[0],m1)){
-               result[0] = dif[0];
-           }
-
-           // 取子节点还有子节点的数组
-           result[1] = dif[1];
-       }else{
-           result = edges[edges.length-1];
+       if (!m2.isEmpty()){
+           // 多个节点的父结点
+           return result2(edges,m1,m2,m3,m4);
        }
-       return result;
+       return  edges[edges.length-1];
+    }
+
+    private int[] result1(int[][] edges,Map<Integer,Integer> m1,Map<Integer,List<Integer>> m2,Map<Integer,Integer> m3,Map<Integer,List<Integer>> m4){
+        int[] result = new int[2];
+        for (Map.Entry<Integer, List<Integer>> entry :m4.entrySet()) {
+            int son = entry.getKey();
+            List<Integer> list = entry.getValue();
+            list.add(m3.get(son));
+
+            Map<Integer,Boolean> circle = new HashMap<>(16);
+            Map<Integer,Boolean> notCircle = new HashMap<>(16);
+            for (Integer root:list) {
+                if (cricle1(new int[]{root,son},root,m1)){
+                    circle.put(root,true);
+                }else{
+                    notCircle.put(root,true);
+                }
+            }
+
+
+            if (circle.size() == 1){
+                for (Integer root: circle.keySet()) {
+                    result[0] = root;
+                    break;
+                }
+                result[1] = son;
+                return result;
+            }else{
+                boolean isEmpty = circle.isEmpty();
+                for (int i=0;i<edges.length;i++) {
+                    // key1->key2
+                    int key1 = edges[i][0];
+                    int key2 = edges[i][1];
+
+                    if (isEmpty){
+                        if (!notCircle.containsKey(key1)){
+                            result[0] = key1;
+                            result[1] = key2;
+                        }
+                    }else {
+                        if (key2 == son && circle.containsKey(key1)){
+                            result[0] = key1;
+                            result[1] = son;
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+        return result;
+    }
+
+    private int[] result2(int[][] edges,Map<Integer,Integer> m1,Map<Integer,List<Integer>> m2,Map<Integer,Integer> m3,Map<Integer,List<Integer>> m4){
+        int[] result = new int[2];
+        int root = 0;
+        List<Integer> nodes = new ArrayList<>();
+        for (Map.Entry<Integer, List<Integer>> entry :m2.entrySet()) {
+            root = entry.getKey();
+            nodes = entry.getValue();
+            nodes.add(m1.get(root));
+            break;
+        }
+
+        Map<Integer,Boolean> circle = new HashMap<>(16);
+        Map<Integer,Boolean> notCircle = new HashMap<>(16);
+        for (Integer node:nodes) {
+            if (cricle1(new int[]{root,node},root,m1)){
+                circle.put(node,true);
+            }else {
+                notCircle.put(node,true);
+            }
+        }
+
+        if (circle.size() == 1){
+            for (int i=0;i<edges.length;i++) {
+                // key1->key2
+                int key1 = edges[i][0];
+                int key2 = edges[i][1];
+                if (!(key1 == root && notCircle.containsKey(key2))){
+                    result[0] = key1;
+                    result[1] = key2;
+                }
+            }
+            return result;
+        }else{
+            for (int i=0;i<edges.length;i++) {
+                // key1->key2
+                int key1 = edges[i][0];
+                int key2 = edges[i][1];
+                if (key1 == root && circle.containsKey(key2)){
+                    result[1] = key2;
+                }
+            }
+            return result;
+        }
     }
 
     private boolean cricle1(int[] dif,int end,Map<Integer,Integer> m){
@@ -259,15 +222,17 @@ public class DigraphSolution {
         int son = dif[1];
         Integer next = son;
         int i = m.size();
+        int hit = 0;
         while (i>=0){
             next = m.get(next);
-            if (next!=null && next.intValue() == end){
+            if ((next!=null && next.intValue() == end)){
+                hit++;
                 break;
             }
             i--;
         }
 
-        if (i<0){
+        if (hit == 0){
             flag = false;
         }
 
