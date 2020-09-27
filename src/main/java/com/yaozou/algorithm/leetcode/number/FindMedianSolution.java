0041fc74178f5475df2112b7d2780fd0286dfe42
yaozou <cbyz.0820>
2020-09-27 15:21:12 +0800

    4.寻找两个正序数组的中位数

--- src/main/java/com/yaozou/algorithm/leetcode/number/FindMedianSolution.java
+++ src/main/java/com/yaozou/algorithm/leetcode/number/FindMedianSolution.java
@@ -8,7 +8,10 @@ package com.yaozou.algorithm.leetcode.number;
  */
 public class FindMedianSolution {
     public static void main(String[] args) {
-
+        FindMedianSolution f = new FindMedianSolution();
+        System.out.println(f.findMedianSortedArrays(new int[]{1, 3},new int[]{2}));
+        System.out.println(f.findMedianSortedArrays(new int[]{},new int[]{1,3, 4}));
+        System.out.println(f.findMedianSortedArrays(new int[]{1,2},new int[]{-1,3}));
     }
 
     /**
@@ -22,14 +25,65 @@ public class FindMedianSolution {
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int num1 = 0;
-        int num2 = 1;
 
         // 中位数，又称中点数，中值。中位数是按顺序排列的一组数据中居于中间位置的数，即在这组数据中，有一半的数据比他大，有一半的数据比他小
         // 出这两个正序数组的中位数
 
+        if (nums1.length == 0&& nums2.length == 0){
+            return 0;
+        }
 
+        // 将两个数组数进行合并并排序
+        int[] array;
+        if (nums2.length == 0 || (nums1.length > 0 && nums1[0] < nums2[0])){
+            array = array(nums1,nums2);
+        }else{
+            array = array(nums2,nums1);
+        }
 
-        return (num1+num2)/2;
+        if (array.length == 1){
+            return array[0];
+        }
+
+        if (array.length % 2 > 0){
+            // 奇数
+            return array[array.length/2];
+        }else{
+            // 偶数
+            int mod = array.length/2;
+            double d1 = array[mod];
+            double d2 = array[mod-1];
+            return (d1+d2)/2;
+        }
+    }
+
+    private int[] array(int[] nums1 , int[] nums2){
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+
+        int[] nums = new int[length1+length2];
+        int j = 0;
+        int m = 0;
+        for (int i = 0;i<length1;i++){
+            int a = nums1[i];
+            for (;j<length2;j++){
+                int b = nums2[j];
+                if (b<=a){
+                    nums[m] = b;
+                    m++;
+                }else{
+                    break;
+                }
+            }
+            nums[m] = a;
+            m++;
+        }
+
+        for (;j<length2;j++){
+            nums[m] = nums2[j];
+            m++;
+        }
+
+        return nums;
     }
 }
