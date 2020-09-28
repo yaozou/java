058c8049ff9b981d5833fc013aebc3de9ce5152a
yaozou <cbyz.0820>
2020-09-28 10:23:59 +0800

    5. 最长回文子串

--- src/main/java/com/yaozou/algorithm/leetcode/string/LongestPalindrome.java
+++ src/main/java/com/yaozou/algorithm/leetcode/string/LongestPalindrome.java
@@ -26,8 +26,11 @@ public class LongestPalindrome {
          * 动态规划的状态转移方程：
          * P(i,j)=P(i+1,j−1)∧(Si==Sj)
          * 也就是说，只有 s[i+1:j−1]是回文串，并且s的第 i和 j个字母相同时，s[i:j]才会是回文串。
+         * 时间复杂度：O(n^2)，其中 n是字符串的长度。动态规划的状态总数为 O(n^2)，对于每个状态，我们需要转移的时间为 O(1)。
+         * 空间复杂度：O(n^2)，即存储动态规划状态需要的空间。
+         *
          */
-
+        // l为长度
         for (int l = 0; l < n; ++l) {
             for (int i = 0; i + l < n; ++i) {
                 int j = i + l;
@@ -47,9 +50,42 @@ public class LongestPalindrome {
         return ans;
     }
 
+    public String longestPalindrome2(String s) {
+        if (s == null || s.length() < 1){
+            return "";
+        }
+        /** 中心扩展算法
+         * 我们枚举每一种边界情况，并从对应的子串开始不断地向两边扩展。
+         * 如果两边的字母相同，我们就可以继续扩展，例如从 P(i+1,j−1)扩展到 P(i,j)；
+         * 如果两边的字母不同，我们就可以停止扩展，因为在这之后的子串都不能是回文串了。
+         *
+         * 时间复杂度：O(n^2)，其中 n 是字符串的长度。长度为 1和 2 的回文中心分别有 n和 n−1个，每个回文中心最多会向外扩展 O(n)次。
+         *
+         */
+        int start = 0,end = 0;
+        for (int i = 0;i<s.length();i++){
+            int len1 = expendAroundCenter(s,i,i);
+            int len2 = expendAroundCenter(s,i,i+1);
+            int max = Math.max(len1,len2);
+            if (max > (end - start)){
+                start = i - (max-1)/2;
+                end   = i+max/2;
+            }
+        }
+        return s.substring(start,end);
+    }
+    private int expendAroundCenter(String s , int left,int right){
+        while (left >= 0 && right<s.length()&& s.charAt(left) == s.charAt(right)){
+            --left;
+            ++right;
+        }
+        return right-left-1;
+    }
+
 
     public String longestPalindrome1(String s) {
         //把相同的词汇或句子,在下文中调换位置或颠倒过来,产生首尾回环的情趣,叫做回文,也叫回环
+        // 自己实现的算法，非常的暴力
         // abbba  abbba
         // abccba
         if(s == null){return null;}
