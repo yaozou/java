5bc45a97b38960ff23e93412f4c89caa04a55edc
yaozou <cbyz.0820>
2020-09-29 10:43:34 +0800

    6. Z 字形变换

--- src/main/java/com/yaozou/algorithm/leetcode/string/ZConvert.java
+++ src/main/java/com/yaozou/algorithm/leetcode/string/ZConvert.java
@@ -17,27 +17,33 @@ public class ZConvert {
     public String convert(String s, int numRows) {
         if (numRows == 1){ return s;}
 
-        List<StringBuilder> rows = new ArrayList<>();
-        for (int i = 0; i < Math.min(numRows, s.length()); i++){rows.add(new StringBuilder());}
-
-        int curRow = 0;
-        boolean goingDown = false;
-
-        for (char c : s.toCharArray()) {
-            rows.get(curRow).append(c);
-            if (curRow == 0 || curRow == numRows - 1) {goingDown = !goingDown;}
-            curRow += goingDown ? 1 : -1;
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0;i<Math.min(s.length(),numRows);i++){
+            list.add(new StringBuilder());
         }
 
-        StringBuilder ret = new StringBuilder();
-        for (StringBuilder row : rows) {
-            ret.append(row);
+        // 是否换行
+        boolean flag = false;
+        int curr = 0;
+        // 每个字母仅会出现一次
+        // 当curr==0时或者curr==numRows-1时，换到下一行
+        for (int i = 0;i<s.length();i++){
+            list.get(curr).append(s.charAt(i));
+            if (curr == 0 || curr == numRows-1 ){flag = !flag;}
+            curr += flag?1:-1;
+            System.out.println(i+"->"+flag+" "+curr+":"+s.charAt(i));
         }
-        return ret.toString();
+
+
+        StringBuilder val = new StringBuilder();
+        for (StringBuilder sb:list) {
+            val.append(sb);
+        }
+        return val.toString();
     }
 
     public static void main(String[] args) {
         ZConvert convert = new ZConvert();
-        System.out.println(convert.convert("LEETCODEISHIRING",3));
+        System.out.println(convert.convert("ABCDEFGHIJKLMN",3));
     }
 }
