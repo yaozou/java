fb77d9f579969cf7495abce610f40250db0586bb
yaozou <cbyz.0820>
2020-10-19 18:37:41 +0800

    21. 合并两个有序链表

---
+++ src/main/java/com/yaozou/algorithm/leetcode/linked/Simple.java
@@ -0,0 +1,24 @@
+package com.yaozou.algorithm.leetcode.linked;
+
+/**
+ * created on 2020/10/19 18:21
+ *
+ * @author yaozou
+ * @since v1.0.0
+ */
+public class Simple {
+    /**
+     * 21. 合并两个有序链表
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+    }
+
+    private class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
