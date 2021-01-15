f8135386250c7e317060b4c5d6437ca6a63db47d
yaozou <cbyz.0820>
2021-01-15 18:23:14 +0800

    逆波兰表达式求值

--- src/main/java/com/yaozou/algorithm/leetcode/stack/MediumSolution.java
+++ src/main/java/com/yaozou/algorithm/leetcode/stack/MediumSolution.java
@@ -1,5 +1,6 @@
 package com.yaozou.algorithm.leetcode.stack;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -82,6 +83,40 @@ public class MediumSolution {
             all.add(list);
         }
         return all;
+    }
+
+    /** 逆波兰表达式求值 */
+    public int evalRPN(String[] tokens) {
+        /**
+         * 逆波兰表达式是一种后缀表达式，所谓后缀就是指算符写在后面。
+         *     平常使用的算式则是一种中缀表达式，如 ( 1 + 2 ) * ( 3 + 4 ) 。
+         *     该算式的逆波兰表达式写法为 ( ( 1 2 + ) ( 3 4 + ) * ) 。
+         *
+         * 逆波兰表达式主要有以下两个优点：
+         *     去掉括号后表达式无歧义，上式即便写成 1 2 + 3 4 + * 也可以依据次序计算出正确结果。
+         *     适合用栈操作运算：遇到数字则入栈；遇到算符则取出栈顶两个数字进行计算，并将结果压入栈中。
+         *
+         */
+        Stack<Integer> stack = new Stack<>();
+        String sub = "-",add = "+",divide = "/",mul="*";
+        for (String token:tokens) {
+            if (sub.equals(token)){
+                int n1 = stack.pop();
+                int n2 = stack.pop();
+                stack.add(n2-n1);
+            }else if (add.equals(token)){
+                stack.add(stack.pop()+stack.pop());
+            }else if (divide.equals(token)){
+                int n1 = stack.pop();
+                int n2 = stack.pop();
+                stack.add(n2/n1);
+            }else if (mul.equals(token)){
+                stack.add(stack.pop()*stack.pop());
+            }else{
+                stack.add(new Integer(token));
+            }
+        }
+        return stack.peek();
     }
 
     private class TreeNode{
