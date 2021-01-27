177ba658b426ea52a8388dfd5af9e70d778069fb
yaozou <cbyz.0820>
2021-01-27 16:35:41 +0800

    扁平化嵌套列表迭代器

--- src/main/java/com/yaozou/algorithm/leetcode/stack/MediumSolution.java
+++ src/main/java/com/yaozou/algorithm/leetcode/stack/MediumSolution.java
@@ -243,6 +243,49 @@ public class MediumSolution {
         }
     }
 
+    public class NestedIterator implements Iterator<Integer> {
+        Deque<Integer> queue = new ArrayDeque<>();
+        public NestedIterator(List<NestedInteger> nestedList) {
+            roll(nestedList);
+        }
+
+        private void roll(List<NestedInteger> nestedList){
+            for (NestedInteger i:nestedList) {
+                if (i.isInteger()) {
+                    queue.add(i.getInteger());
+                } else {
+                    roll(i.getList());
+                }
+            }
+        }
+
+        @Override
+        public Integer next() {
+            return queue.pop();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !queue.isEmpty();
+        }
+    }
+
+      // This is the interface that allows for creating nested lists.
+      // You should not implement it, or speculate about its implementation
+      public interface NestedInteger {
+
+          // @return true if this NestedInteger holds a single integer, rather than a nested list.
+         public boolean isInteger();
+
+          // @return the single integer that this NestedInteger holds, if it holds a single integer
+          // Return null if this NestedInteger holds a nested list
+          public Integer getInteger();
+
+          // @return the nested list that this NestedInteger holds, if it holds a nested list
+          // Return null if this NestedInteger holds a single integer
+          public List<NestedInteger> getList();
+      }
+
     public static void main(String[] args) {
         MediumSolution solution = new MediumSolution();
         // {1,0},{0,1},{1,1}
