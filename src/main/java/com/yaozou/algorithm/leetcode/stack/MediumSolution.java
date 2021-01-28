78445e707bb86e8d9ae48a1d6342d3aa28ca5cde
yaozou <cbyz.0820>
2021-01-28 11:44:25 +0800

    132模式

--- src/main/java/com/yaozou/algorithm/leetcode/stack/MediumSolution.java
+++ src/main/java/com/yaozou/algorithm/leetcode/stack/MediumSolution.java
@@ -353,6 +353,25 @@ public class MediumSolution {
         return sb.length() == 0?"0":sb.toString();
     }
 
+    public boolean find132pattern(int[] nums) {
+        if (nums.length < 3){return false;}
+
+        int[] mins = new int[nums.length];
+        mins[0]    = nums[0];
+        for (int i = 1;i<nums.length;i++) {
+            mins[i] = Math.min(mins[i-1],nums[i]);
+        }
+        Stack<Integer> stack = new Stack<>();
+        for (int j = nums.length - 1; j >= 0; j--) {
+            if (nums[j] > mins[j]) {
+                while (!stack.isEmpty() && stack.peek() <= mins[j]) { stack.pop();}
+                if (!stack.isEmpty() && stack.peek() < nums[j]) {return true;}
+                stack.push(nums[j]);
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         MediumSolution solution = new MediumSolution();
         // {1,0},{0,1},{1,1}
